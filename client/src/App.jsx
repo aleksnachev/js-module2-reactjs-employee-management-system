2222427576be97557d37822ba0c23e1fb13cc459
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-09 19:06:30 +0200

    move get users to app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
 import Pagination from "./components/Pagination.jsx"
@@ -8,6 +8,17 @@ import CreateUserModal from "./components/CreateUseModal.jsx"
 
 function App() {
     const [showCreateUser, setShowCreateUser] = useState(false)
+    const [users,setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/users')
+            .then(res => res.json())
+            .then(result => {
+                setUsers(Object.values(result));
+            })
+            .catch((err) => alert(err.message))
+    }, [])
+
 
     const addUserClickHandler = () =>{
         setShowCreateUser(true)
@@ -21,7 +32,16 @@ function App() {
         event.preventDefault()
 
         const formData = new FormData(event.target)
-        const userData = Object.fromEntries(formData)
+        const {country,city,street,streetNumber, ...userData} = Object.fromEntries(formData)
+        userData.address ={
+            country,
+            city,
+            street,
+            streetNumber
+        }
+
+        userData.createdAt = new Date().toISOString()
+        userData.createdAt = new Date().toISOString()
 
         fetch('http://localhost:3030/jsonstore/users', {
             method: 'POST',
@@ -30,8 +50,8 @@ function App() {
             },
             body: JSON.stringify(userData)
         })
-            .then(res => res.json())
-            .then(data => {console.log(res)})
+            .then(res => res.json()) 
+            .then(data => {console.log(data)})
     }
     return (
         <div>
@@ -41,7 +61,7 @@ function App() {
                 <section className="card users-container">
                     <Search/>
 
-                    <UserList/>
+                    <UserList users={users}/>
 
                     <button className="btn-add btn" onClick = {addUserClickHandler}>Add new user</button>
 
