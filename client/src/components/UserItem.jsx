ffcf9a09e37cbee85b44e6e7c08e5894e4b0df6b
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-09 18:14:12 +0200

    convert iso date to formatted date

--- client/src/components/UserItem.jsx
+++ client/src/components/UserItem.jsx
@@ -1,3 +1,5 @@
+import { fromIsoDate } from "../utils/dateTimeutils.js";
+
 export default function UserItem({
     _id,
     firstName,
@@ -18,7 +20,7 @@ export default function UserItem({
             <td>{lastName}</td>
             <td>{email}</td>
             <td>{phoneNumber}</td>
-            <td>{createdAt}</td>
+            <td>{fromIsoDate(createdAt)}</td>
             {/* <td>June 28, 2022</td> */}
 
             <td className="actions">
